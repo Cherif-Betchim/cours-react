77c06fdd4dda2a0daa614be4c19bc70dd8ff4646
Amandine Cr <2412770-AmandineCr@users.noreply.gitlab.com>
2020-11-23 14:55:26 +0100

    spirale done

--- assets/javascript.test.js
+++ assets/javascript.test.js
@@ -59,78 +59,78 @@ const { anagrammes, Stack, spirale, puissance4, fizzBuzz } = require('./javascri
 // });
 //
 //
-// describe('La fonction matrice', () => {
-//   test('doit exister', () => {
-//     expect(spirale).toBeDefined();
-//     expect(typeof spirale).toEqual('function');
-//   });
-//
-//   test('doit fournir la matrice de taille 2x2', () => {
-//     const m = spirale(2);
-//     expect(m.length).toEqual(2);
-//     expect(m[0]).toEqual([1, 2]);
-//     expect(m[1]).toEqual([4, 3]);
-//   });
-//
-//   test('et celle de taille 3x3', () => {
-//     const m = spirale(3);
-//     expect(m.length).toEqual(3);
-//     expect(m[0]).toEqual([1, 2, 3]);
-//     expect(m[1]).toEqual([8, 9, 4]);
-//     expect(m[2]).toEqual([7, 6, 5]);
-//   });
-//
-//   test('et celle de taille 4x4', () => {
-//     const m = spirale(4);
-//     expect(m.length).toEqual(4);
-//     expect(m[0]).toEqual([1, 2, 3, 4]);
-//     expect(m[1]).toEqual([12, 13, 14, 5]);
-//     expect(m[2]).toEqual([11, 16, 15, 6]);
-//     expect(m[3]).toEqual([10, 9, 8, 7]);
-//   });
-// });
-//
-//
-describe('La fonction FizzBuzz', () => {
-  beforeEach(() => {
-    jest.spyOn(console, 'log');
-  });
-
-  afterEach(() => {
-    console.log.mockRestore();
-  });
-
+describe('La fonction matrice', () => {
   test('doit exister', () => {
-    expect(fizzBuzz).toBeDefined();
-    expect(typeof fizzBuzz).toEqual('function');
+    expect(spirale).toBeDefined();
+    expect(typeof spirale).toEqual('function');
   });
 
-  test('doit afficher 5 expressions quand appelé avec 5', () => {
-    fizzBuzz(5);
-
-    expect(console.log.mock.calls.length).toEqual(5);
+  test('doit fournir la matrice de taille 2x2', () => {
+    const m = spirale(2);
+    expect(m.length).toEqual(2);
+    expect(m[0]).toEqual([1, 2]);
+    expect(m[1]).toEqual([4, 3]);
   });
 
-  test('doit afficher les bonnes valeurs', () => {
-    fizzBuzz(15);
+  test('et celle de taille 3x3', () => {
+    const m = spirale(3);
+    expect(m.length).toEqual(3);
+    expect(m[0]).toEqual([1, 2, 3]);
+    expect(m[1]).toEqual([8, 9, 4]);
+    expect(m[2]).toEqual([7, 6, 5]);
+  });
 
-    expect(console.log.mock.calls[0][0]).toEqual(1);
-    expect(console.log.mock.calls[1][0]).toEqual(2);
-    expect(console.log.mock.calls[2][0]).toEqual('fizz');
-    expect(console.log.mock.calls[3][0]).toEqual(4);
-    expect(console.log.mock.calls[4][0]).toEqual('buzz');
-    expect(console.log.mock.calls[5][0]).toEqual('fizz');
-    expect(console.log.mock.calls[6][0]).toEqual(7);
-    expect(console.log.mock.calls[7][0]).toEqual(8);
-    expect(console.log.mock.calls[8][0]).toEqual('fizz');
-    expect(console.log.mock.calls[9][0]).toEqual('buzz');
-    expect(console.log.mock.calls[10][0]).toEqual(11);
-    expect(console.log.mock.calls[11][0]).toEqual('fizz');
-    expect(console.log.mock.calls[12][0]).toEqual(13);
-    expect(console.log.mock.calls[13][0]).toEqual(14);
-    expect(console.log.mock.calls[14][0]).toEqual('fizzbuzz');
+  test('et celle de taille 4x4', () => {
+    const m = spirale(4);
+    expect(m.length).toEqual(4);
+    expect(m[0]).toEqual([1, 2, 3, 4]);
+    expect(m[1]).toEqual([12, 13, 14, 5]);
+    expect(m[2]).toEqual([11, 16, 15, 6]);
+    expect(m[3]).toEqual([10, 9, 8, 7]);
   });
 });
+//
+//
+// describe('La fonction FizzBuzz', () => {
+//   beforeEach(() => {
+//     jest.spyOn(console, 'log');
+//   });
+//
+//   afterEach(() => {
+//     console.log.mockRestore();
+//   });
+//
+//   test('doit exister', () => {
+//     expect(fizzBuzz).toBeDefined();
+//     expect(typeof fizzBuzz).toEqual('function');
+//   });
+//
+//   test('doit afficher 5 expressions quand appelé avec 5', () => {
+//     fizzBuzz(5);
+//
+//     expect(console.log.mock.calls.length).toEqual(5);
+//   });
+//
+//   test('doit afficher les bonnes valeurs', () => {
+//     fizzBuzz(15);
+//
+//     expect(console.log.mock.calls[0][0]).toEqual(1);
+//     expect(console.log.mock.calls[1][0]).toEqual(2);
+//     expect(console.log.mock.calls[2][0]).toEqual('fizz');
+//     expect(console.log.mock.calls[3][0]).toEqual(4);
+//     expect(console.log.mock.calls[4][0]).toEqual('buzz');
+//     expect(console.log.mock.calls[5][0]).toEqual('fizz');
+//     expect(console.log.mock.calls[6][0]).toEqual(7);
+//     expect(console.log.mock.calls[7][0]).toEqual(8);
+//     expect(console.log.mock.calls[8][0]).toEqual('fizz');
+//     expect(console.log.mock.calls[9][0]).toEqual('buzz');
+//     expect(console.log.mock.calls[10][0]).toEqual(11);
+//     expect(console.log.mock.calls[11][0]).toEqual('fizz');
+//     expect(console.log.mock.calls[12][0]).toEqual(13);
+//     expect(console.log.mock.calls[13][0]).toEqual(14);
+//     expect(console.log.mock.calls[14][0]).toEqual('fizzbuzz');
+//   });
+// });
 //
 //
 // describe('La fonction puissance4', () => {
